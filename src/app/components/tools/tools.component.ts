798def2da08bb63d29b1deaa1d086e320d6d6d9e
jrakibi <j.errakibi@gmail.com>
2024-01-18 20:20:27 +0100

    Add tools component and visuals component

--- src/app/components/tools/tools.component.ts
+++ src/app/components/tools/tools.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Tool } from 'src/app/models/tool.model';
+import { DataService } from 'src/app/services/data.service';
+
+
 
 @Component({
   selector: 'app-tools',
   templateUrl: './tools.component.html',
-  styleUrls: ['./tools.component.css']
+  styleUrls: ['./tools.component.scss']
 })
 export class ToolsComponent implements OnInit {
 
-  constructor() { }
+  tools: Tool[] = [];
+
+  loading: boolean = false;
+  error: string = '';
+
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.loading = true;
+    this.dataService.fetchData<any[]>('assets/data/tools.json').subscribe(
+      data => {
+        this.tools = data;
+        this.loading = false;
+      },
+      error => {
+        this.error = 'Failed to load tools data.';
+        this.loading = false;
+      }
+    );
   }
 
 }
