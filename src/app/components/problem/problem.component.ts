9e5324e72b1b024b8a179b66c47d90d85c805cf6
jrakibi <j.errakibi@gmail.com>
2024-01-18 22:03:01 +0100

    Update problem and tools components with loading and error states

--- src/app/components/problem/problem.component.ts
+++ src/app/components/problem/problem.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Problem } from 'src/app/models/problem.model';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-problem',
@@ -6,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./problem.component.scss']
 })
 export class ProblemComponent implements OnInit {
-  ngOnInit(): void {
-  }
+  exercise: Problem | null = null;
+  loading: boolean = false;
+  error: string = '';
 
-    
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.dataService.fetchData<Problem>('assets/data/problems.json').subscribe(
+      data => {
+        this.exercise = data;
+        this.loading = false;
+      },
+      error => {
+        this.error = 'Failed to load exercise data.';
+        this.loading = false;
+      }
+    );
+  }
 }
