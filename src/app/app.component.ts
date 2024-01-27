689bd0006ce5a18d5c079c1207778d1022ead38d
jrakibi <j.errakibi@gmail.com>
2024-01-27 20:33:56 +0100

    Add responsive mobile warning message and Bitcoin logo image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,8 +16,32 @@ export class AppComponent implements OnInit {
     showModal = false;
 
     ngOnInit(): void {
+      this.checkDeviceSize();
+
       setTimeout(() => {
         this.showModal = true;
       }, 5000);
+    }
+
+
+
+    title = 'BTC Illustrated';
+    isMobile: boolean = false;
+    showMobileWarning: boolean = true;
+    showHeader = true;
+  
+  
+    checkDeviceSize() {
+      const mobileBreakpoint = 768; // You can adjust this value
+      this.isMobile = window.innerWidth < mobileBreakpoint;
+      this.showMobileWarning = this.isMobile; // Show warning initially only on mobile
+    }
+  
+    continueAnyway() {
+      this.showMobileWarning = false; // Hide mobile warning and show platform content
+    }
+  
+    hideHeader() {
+      this.showHeader = false;
     }
   }
